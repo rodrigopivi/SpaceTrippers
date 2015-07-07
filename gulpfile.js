52d28f13484716a02c70779ebd49432f09de335b
Rodrigo <sr.rodrigopv@gmail.com>
2015-07-07 14:00:40 -0500

    better gulp tsc

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
 var gulp    = require('gulp');
 var tslint  = require('gulp-tslint');
-var exec    = require('child_process').exec;
+var typescript = require('gulp-tsc');
 var gulp    = require('gulp-help')(gulp);
 
-gulp.task('tslint', 'Lints all TypeScript source files', function(){
+gulp.task('tslint', 'Lints all TypeScript source files', function () {
   return gulp.src('src/**/*.ts')
   .pipe(tslint())
   .pipe(tslint.report('verbose'));
 });
 
-gulp.task('build', 'Compiles all TypeScript source files', function (cb) {
-  exec('tsc', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
+gulp.task('build','Compiles all TypeScript source files', function () {
+  gulp.src(['src/**/*.ts'])
+    .pipe(typescript({
+    	module: "CommonJS",
+		sourceMap: true,
+		declaration: true,
+		out: "app.js"
+    }))
+    .pipe(gulp.dest('dist/'))
 });
